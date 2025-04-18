72635df1495361192683bec1e58bbdef907a0cf4
Andrii Nazarov <andreu.nazarov@gmail.com>
2025-04-18 16:49:33 +0200

    -initial commit

---
+++ src/test/java/com/nazarov/footballmanager/FootballmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nazarov.footballmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
