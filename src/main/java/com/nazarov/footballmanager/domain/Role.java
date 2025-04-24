a453a258f3b91d22836a8cfeb15e5ea1dad0f15f
Andrii Nazarov <andreu.nazarov@gmail.com>
2025-04-24 15:43:47 +0200

    add User and Role entities with repositories for user management

---
+++ src/main/java/com/nazarov/footballmanager/domain/Role.java
@@ -0,0 +1,27 @@
+package com.nazarov.footballmanager.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "role_id")
+  private Integer id;
+
+  @Column(name = "role_name", length = 50, unique = true, nullable = false)
+  private String name;
+}
